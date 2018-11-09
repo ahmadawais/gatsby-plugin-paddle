ca242c879af3b6aa515930b6f01347e98a3f5cb7
Ahmad Awais <me@AhmadAwais.com>
2018-11-09 19:13:29 +0500

    📦 NEW: Option Vendor ID

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -12,7 +12,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  * @link https://paddle.com/docs/paddle-checkout-web/
  */
 exports.onRenderBody = function (_ref, pluginOptions) {
-  var setPostBodyComponents = _ref.setPostBodyComponents;
+	var setPostBodyComponents = _ref.setPostBodyComponents;
 
-  return setPostBodyComponents([_react2.default.createElement("script", { key: "gatsby-plugin-paddle", src: "https://cdn.paddle.com/paddle/paddle.js" })]);
+	if (process.env.NODE_ENV === "production") {
+		return setPostBodyComponents([_react2.default.createElement("script", { key: "gatsby-plugin-paddle", src: "https://cdn.paddle.com/paddle/paddle.js" }), _react2.default.createElement("script", {
+			key: "gatsby-plugin-paddle-vendor",
+			type: "text/javascript",
+			dangerouslySetInnerHTML: { __html: "Paddle.Setup({ vendor: " + pluginOptions.vendorId + " });" }
+		})]);
+	}
 };
